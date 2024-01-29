825a0ee73e5c4fadbd2ba832ade37e83c331869b
leven <leven890619@gmail.com>
2024-01-29 16:33:38 +0800

    update contract address

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,3 @@
-import { NewNFTCreateds } from "@/lib/type";
-import { Coll1, Coll2, Coll3, Coll4, Coll5, Coll6 } from "@/public/images";
-
 export const BRCROWD_DOC = 'https://docs.becrowd.xyz/'
 export const TWITTER_ADDRESS = 'https://twitter.com/BeCrowd_'
 export const GITHUB_ADDRESS = 'https://github.com/Crowd-Labs'
@@ -13,10 +10,10 @@ export const ELEMENT_MARKET = 'https://element.market/'
 
 export const BeCrowdHubProxy = 'BeCrowdHubProxy'
 export const BeCrowd_WEBSITE = 'https://www.becrowd.xyz'
-export const FREE_DERIVIED_MODULE_ADDRESS = '0x1EF1D6C5883f7093Fd51ec15CeB3C28A6aE074a8'
-export const FEE_DERIVIED_MODULE_ADDRESS = '0x84DC4d9e99e74Ce9cc56c8F245a4dc36B4460094'
-export const WHITELIST_FREE_DERIVIED_MODULE_ADDRESS = '0x2e4F1A206a3e3747d7AAb77d29a69a918Cd070E9'
-export const WHITELIST_FEE_DERIVIED_MODULE_ADDRESS = '0x137B5E6Bfe8C54c24b667938376A89139B20fD99'
+export const FREE_DERIVIED_MODULE_ADDRESS = '0x1212c1fb86514080F40cfc59C5D82FCbeB469702'
+export const FEE_DERIVIED_MODULE_ADDRESS = '0xcD22ff5d299AB3cC5e838c31723DaFcb512857D2'
+export const WHITELIST_FREE_DERIVIED_MODULE_ADDRESS = '0xc480266AC2F4c975C44bD075B5B3cEF937Cce92D'
+export const WHITELIST_FEE_DERIVIED_MODULE_ADDRESS = '0x3d50830B330c5D436D2d7ade7266cc9B4173F6c6'
 
 export const navLinks = [
     { href: "/collection", label: "Collection", target: "_self" },
