c516d5159a05b854ad34bb064a24e559b7a55d8a
leoliq <hualanice@gmail.com>
2024-02-03 17:07:14 +0800

    fixed nested link issue

--- app/collection/collections.tsx
+++ app/collection/collections.tsx
@@ -3,28 +3,32 @@ import { getAllCollectionInfo } from '@/api/thegraphApi';
 import BuyButton from '@/components/Button/BuyBtn';
 import { CollectionDone } from '@/components/Collection/CollectionCards';
 import { CollectionInfo } from '@/lib/type';
-import Link from 'next/link';
+
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 
 const Collections = () => {
     const [collections, setCollections] = useState<CollectionInfo[]>([])
-    useEffect(()=>{
-        getAllCollectionInfo().then(res=>{
+    useEffect(() => {
+        getAllCollectionInfo().then(res => {
             setCollections(res as CollectionInfo[])
         })
     }, [])
+    const router = useRouter()
     return (
         <div className='grid grid-cols-4 gap-4 py-8'>
             {collections?.map(card => (
-                <Link key={card.collectionId} href={`/collection/${card?.derivedCollectionAddr}`}>
+                <div key={card.collectionId} onClick={() => {
+                    router.push(`/collection/${card?.derivedCollectionAddr}`)
+                }}>
                     <CollectionDone sampleData={card} >
                         <div className="absolute w-full bottom-0 h-11 flex items-center justify-between bg-green px-2 text-white gap-2">
                             <div>{card.name}</div>
                             <BuyButton data={card as any} />
                         </div>
                     </CollectionDone>
-                </Link>
+                </div>
             ))}
         </div>
     )
