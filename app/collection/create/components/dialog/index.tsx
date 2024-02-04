51826a88093b010d2dbfac82ff217636aae425e8
leven <leven890619@gmail.com>
2024-02-04 17:39:24 +0800

    fix stake amount

--- app/collection/create/components/dialog/index.tsx
+++ app/collection/create/components/dialog/index.tsx
@@ -16,7 +16,7 @@ const DialogConfirm = (props: DialogConfirm) => {
         <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
         <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[900px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-[#161b10] p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title className="text-white my-8 text-2xl font-medium text-center">
-            Are you sure to stake <span className='text-egg'>0.001 Eth</span>  to create a collection for free?
+            Are you sure to stake <span className='text-egg'>0.05 Eth</span>  to create a collection for free?
           </Dialog.Title>
           <Dialog.Description className="text-white mt-8 text-[15px] leading-normal text-center">
             You can get back your staked Eth and yield(<span className='text-egg'>From Blast</span>) back after one week!
